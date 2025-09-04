dcf45801210372f98026675145885f48ab93d83f
Rosalía Fernández <rosaliafernandez138@gmail.com>
2025-09-04 08:36:20 -0300

    Obtener elementos del DOM

---
+++ js/almacenar.js
@@ -0,0 +1,5 @@
+// Obtener elementos del DOM
+const inputItem = document.getElementById('item');
+const botonAgregar = document.getElementById('agregar');
+const botonLimpiar = document.getElementById('limpiar');
+const contenedor = document.getElementById('contenedor');
